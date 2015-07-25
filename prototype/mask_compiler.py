030361fd376c305bcc8bf133070581345a63542c
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-07-26 00:20:09 +0200

    proper namescope

--- prototype/mask_compiler.py
+++ prototype/mask_compiler.py
@@ -7,19 +7,27 @@ import re
 import mask_ir
 
 text = """
-d = k
-k = $d
-rule test
-  command = abc $in $out $k
-  e = f
 
-# test
+cflags = /nologo /showIncludes
+ldflags = /nologo
 
-build a.txt:test b.txt
-#  lol = wow
-#  lol2 = wow2
-e = d
-build c.txt:test d.txt
+k = zz
+
+rule cc
+  command = cl $in $cflags /Fo$out t=$k
+  description = compile
+
+rule link
+  command = cl $in /link $ldflags /out:$out
+  description = link
+
+k = a.obj
+
+build $k:cc a.cpp
+  k = lool
+build b.obj:cc b.cpp
+  k = lool2
+build result.exe:link a.cpp b.cpp
 
 """
 
@@ -28,6 +36,7 @@ class Namescope:
 		self.vars = []
 		self.rules = []
 		self.builds = []
+		self.cmds = []
 
 	def var(self, var):
 		self.vars.append(var)
@@ -45,46 +54,48 @@ class Namescope:
 		raise ValueError("cant find rule " + name)
 
 	def get_var_in_scope(self, vars, name):
-		for var in vars:
+		for var in reversed(vars):
 			if name == var["to"]:
 				return var
 		raise ValueError("cant find var in scope " + name)
 
-	def get_var_value(self, vars, name, lookup = []):
-		var = self.get_var_in_scope(vars, name)
-		val = var["val"]
-
+	def get_varstr_value(self, vars, val, lookup = []):
 		sub_vars = re.finditer("\$([a-zA-Z0-9_.-]+)", val)
-
 		for sub_var in reversed(list(sub_vars)):
 			name = sub_var.group(1)
 			if name in lookup:
 				raise ValueError("recursive lookup for " + name + " with lookup stack " + str(lookup))
 			sub_val = self.get_var_value(vars, name, lookup + [name])
 			val = val[0:sub_var.start()] + sub_val + val[sub_var.end():]
-
 		return val
 
+	def get_var_value(self, vars, name, lookup = []):
+		var = self.get_var_in_scope(vars, name)
+		return self.get_varstr_value(vars, var["val"], lookup)
 
 	def compile(self):
 		for build in self.builds:
-			print(build)
-
 			rule = self.get_rule(build["build"]["rule_name"])
+			rule_vars = self.vars[:rule["var_index"]]
 
-			vars = []
-			vars.extend(self.vars[:rule["var_index"]])
+			build_vars = self.vars[:build["var_index"]]
 
-			if build["build"]["vars"]: # you need to evaluate them with build var_index scope
-				print("probably this doesn't work fully yet")
-				vars.extend(build["build"]["vars"])
+			if build["build"]["vars"]:
+				build_nested_vars = build["build"]["vars"]
+				if build_nested_vars is not list:
+					build_nested_vars = [build_nested_vars]
+				for var in build_nested_vars:
+					rule_vars.append({"to": var["to"], "val": self.get_varstr_value(build_vars, var["val"])})
 
-			vars.append({"to": "in", "val": " ".join(build["build"]["inputs"])})
-			vars.append({"to": "out", "val": " ".join(build["build"]["outputs"])})
-			vars.extend(rule["rule"]["vars"])
+			inputs = [self.get_varstr_value(build_vars, input) for input in build["build"]["inputs"]]
+			outputs = [self.get_varstr_value(build_vars, output) for output in build["build"]["outputs"]]
 
-			cmd = self.get_var_value(vars, "command")
-			print(cmd)
+			rule_vars.append({"to": "in", "val": " ".join(inputs)})
+			rule_vars.append({"to": "out", "val": " ".join(outputs)})
+			rule_vars.extend(rule["rule"]["vars"])
+
+			cmd = self.get_var_value(rule_vars, "command")
+			self.cmds.append({"cmd": cmd, "inputs": inputs, "outputs": outputs})
 
 namescope = Namescope()
 parser = mask_ir.mask_ir_Parser()
@@ -101,3 +112,5 @@ for expr in ast:
 		print("unknown " + str(expr))
 
 namescope.compile()
+
+pprint(namescope.cmds)
