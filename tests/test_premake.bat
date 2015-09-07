eee39a76607bc0e119725610d9f26e0e7718e0c9
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-09-07 17:55:50 +0200

    was able to compile premake-core

--- tests/test_premake.bat
+++ tests/test_premake.bat
@@ -2,7 +2,8 @@
 cd premake\test
 premake5 --scripts=..\premake-ninja ninja
 cd build
-rem python ..\..\..\..\mask.py --verbose build.ninja qmake/build.pro
-python ..\..\..\..\mask.py --verbose build.ninja build.bat
+python ..\..\..\..\mask.py --verbose build.ninja build.pro
+rem python ..\..\..\..\mask.py --verbose build.ninja build.bat
+C:\Qt5.4\5.4\msvc2013_opengl\bin\qmake -r -tp vc build.pro
 cd ..\..\..
 
