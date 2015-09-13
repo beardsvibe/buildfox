76a33943464b088781eb06cbf7256ee8cf9a364a
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-09-13 19:38:03 +0200

    more

--- buildfox/poc/buildfox.py
+++ buildfox/poc/buildfox.py
@@ -23,15 +23,61 @@ if args.get("workdir"):
 	os.chdir(args.get("workdir"))
 
 # ----------------------------------------------------------- parsing
+def get_paths(income, outcome = None):
+	if outcome:
+		pprint(income)
+		pprint(outcome)
+		return income, outcome
+	return income
+
+def get_vars(expr):
+	for var in expr.get("vars"):
+		yield "  %s = %s\n" % (var.get("assign"), var.get("value"))
+def do_expr(expr):
+	if "assign" in expr:
+		return "%s = %s\n" % (expr.get("assign"), expr.get("value"))
+	elif "rule" in expr:
+		return "rule %s\n%s" % (expr.get("rule"), get_vars(expr))
+	elif "build" in expr:
+		inputs, outputs = get_paths(expr.get("inputs_explicit"), expr.get("targets_explicit"))
+		inputs_implicit = get_paths(expr.get("inputs_implicit"))
+		inputs_order = get_paths(expr.get("inputs_order"))
+		output = "build %s: %s" % (" ".join(outputs), expr.get("build"))
+		if inputs:
+			output += " " + " ".join(inputs)
+		if inputs_implicit:
+			output += " | " + " ".join(inputs_implicit)
+		if inputs_order:
+			output += " | " + " ".join(inputs_order)
+		return "%s\n%s" % (output, get_vars(expr))
+	elif "filter" in expr:
+		print("TODO support filter")
+		pprint(expr)
+		return ""
+	elif "defaults" in expr:
+		return "defaults %s\n" % (" ".join(get_paths(expr.get("defaults"))))
+	elif "pool" in expr:
+		return "pool %s\n%s" % (expr.get("pool"), get_vars(expr))
+	elif "include" in expr:
+		print("TODO support include")
+		pprint(expr)
+		return ""
+	elif "subninja" in expr:
+		print("TODO support subninja")
+		pprint(expr)
+		return ""
+	else:
+		raise ValueError("unknown ast expr " + str(expr))
+
 def do_file(filename):
 	with open(filename, "r") as f:
 		input_text = f.read()
 		parser = fox_Parser(parseinfo = False)
 		ast = parser.parse(input_text, "manifest", trace = False, whitespace = string.whitespace, nameguard = True)
-
-		pprint(ast)
-
-	return "hello there"
+		output = ""
+		for expr in ast:
+			output += do_expr(expr)
+		return output
 
 output_text = do_file(args.get("in"))
 
