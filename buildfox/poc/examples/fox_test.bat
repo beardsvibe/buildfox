dbc65356261d698ddeb349c948a5c8a5369129bb
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-09-28 22:48:03 +0200

    remove old stuff

--- buildfox/poc/examples/fox_test.bat
+++
@@ -1,4 +0,0 @@
-python ..\buildfox.py -i fox_test.fox -o fox_test.ninja -d toolset msvc -d variation debug
-python ..\buildfox.py -i fox_test.fox -o fox_test_gcc.ninja -d toolset gcc -d variation debug
-ninja -f fox_test.ninja
-ninja -f fox_test_gcc.ninja
