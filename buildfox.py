f6c0b1af695c658bc44d54c41dd7521506f15cfa
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-10-01 23:16:41 +0200

    test

--- buildfox.py
+++ buildfox.py
@@ -851,6 +851,9 @@ class Engine:
 		self.rules[rule_name] = vars
 
 	def build(self, obj, assigns):
+		from pprint import pprint
+		pprint(obj)
+
 		inputs_explicit, targets_explicit = self.eval_path(obj[3], obj[0])
 		targets_implicit = self.eval_path(obj[1])
 		rule_name = self.eval(obj[2])
