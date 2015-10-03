660bb37e40ffc478aa77a1379515fa00398266f1
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-10-04 00:33:45 +0200

    parser tests

--- tests/parser.py
+++ tests/parser.py
@@ -1,21 +1,33 @@
 # Parser test suite
 
+import os
 import sys
 import json
+import glob
+import traceback
+from pprint import pprint
+from deepdiff import DeepDiff # pip install deepdiff
 
 sys.path.append("..")
-
 from lib_parser import parse
 
 class EngineMock:
 	def __init__(self):
 		self.output = []
 
+	def fix_assigns(self, assign):
+		return [list(t) for t in assign]
+
 	def on_comment(self, comment):
-		self.output.append({"comment": comment})
+		self.output.append({
+			"comment": comment
+		})
 
 	def on_rule(self, obj, assigns):
-		self.output.append({"rule": obj, "assigns": assigns})
+		self.output.append({
+			"rule": obj,
+			"assigns": self.fix_assigns(assigns)
+		})
 
 	def on_build(self, obj, assigns):
 		self.output.append({
@@ -25,32 +37,81 @@ class EngineMock:
 			"inputs_explicit": obj[3],
 			"inputs_implicit": obj[4],
 			"inputs_order": obj[5],
-			"assigns": assigns
+			"assigns": self.fix_assigns(assigns)
 		})
 
 	def on_default(self, obj, assigns):
-		self.output.append({"default": obj, "assigns": assigns})
+		self.output.append({
+			"default": obj,
+			"assigns": self.fix_assigns(assigns)
+		})
 
 	def on_pool(self, obj, assigns):
-		self.output.append({"pool": obj, "assigns": assigns})
+		self.output.append({
+			"pool": obj,
+			"assigns": self.fix_assigns(assigns)
+		})
 
 	def filter(self, obj):
-		self.output.append({"filter": obj})
+		self.output.append({
+			"filter": obj
+		})
 
 	def on_auto(self, obj, assigns):
-		self.output.append({"auto": obj[1], "outputs": obj[0], "inputs": obj[2], "assigns": assigns})
+		self.output.append({
+			"auto": obj[1],
+			"outputs": obj[0],
+			"inputs": obj[2],
+			"assigns": self.fix_assigns(assigns)
+		})
 
 	def on_print(self, obj):
-		self.output.append({"print": obj})
+		self.output.append({
+			"print": obj
+		})
 
 	def on_assign(self, obj):
-		self.output.append({"assign": obj})
+		self.output.append({
+			"assign": list(obj)
+		})
 
 	def on_transform(self, obj):
-		self.output.append({"transform": obj[0], "pattern": obj[1]})
+		self.output.append({
+			"transform": obj[0],
+			"pattern": obj[1]
+		})
 
 	def on_include(self, obj):
-		self.output.append({"include": obj})
+		self.output.append({
+			"include": obj
+		})
 
 	def on_subninja(self, obj):
-		self.output.append({"subninja": obj})
+		self.output.append({
+			"subninja": obj
+		})
+
+any_failed = False
+for test_filename in glob.glob("parser/*.in"):
+	print("---------------------------------------- testing %s" % test_filename)
+	try:
+		with open(os.path.splitext(test_filename)[0] + ".json", "r") as f:
+			reference = json.loads(f.read())
+		engine = EngineMock()
+		parse(engine, test_filename)
+		diff = DeepDiff(reference, engine.output)
+		if diff:
+			print("results are differ from reference : ")
+			pprint(diff)
+			any_failed = True
+	except:
+		err = sys.exc_info()[0]
+		print("exception error : %s" % err)
+		traceback.print_exc()
+		any_failed = True
+
+if any_failed:
+	print("one or more tests failed")
+	sys.exit(1)
+else:
+	print("done")
