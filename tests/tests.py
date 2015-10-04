0be30e59764d36b119c1a596ae1c322dee97ff94
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-10-04 11:57:35 +0200

    add arguments to tests

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import glob
+import argparse
 import traceback
 from pprint import pprint
 from deepdiff import DeepDiff # pip install deepdiff
@@ -92,39 +93,62 @@ class EngineMock:
 			"subninja": obj
 		})
 
-any_failed = False
-for test_filename in glob.glob("suite/*.fox"):
+def run_test(test_filename, print_json = False, print_ninja = False):
 	print("---------------------------------------- testing %s" % test_filename)
 	try:
 		with open(os.path.splitext(test_filename)[0] + ".json", "r") as f:
 			reference = json.loads(f.read())
 		engine = EngineMock()
 		parse(engine, test_filename)
+		if print_json:
+			print("------------ json")
+			pprint(engine.output)
+			print("------------ json end")
 		diff = DeepDiff(reference, engine.output)
 		if diff:
 			print("results are differ from reference : ")
 			pprint(diff)
-			any_failed = True
+			return False
 
 		with open(os.path.splitext(test_filename)[0] + ".ninja", "r") as f:
 			reference = f.read()
 		engine = Engine()
 		engine.load(test_filename, logo = False)
+		if print_ninja:
+			print("------------ ninja")
+			pprint(engine.text())
+			print("------------ ninja end")
 		diff = DeepDiff(reference, engine.text())
 		if diff:
 			print("results are differ from reference : ")
 			pprint(diff)
-			any_failed = True
+			return False
+
+		return True
 	except:
 		err = sys.exc_info()[0]
 		print("exception error : %s" % err)
 		traceback.print_exc()
-		any_failed = True
+		return False
+
+argsparser = argparse.ArgumentParser(description = "buildfox test suite")
+argsparser.add_argument("-i", "--in", help = "test inputs", default = "suite/*.fox")
+argsparser.add_argument("--dry", action = "store_true",
+	help = "ignore tests failures", default = False, dest = "dry")
+argsparser.add_argument("--json", action = "store_true",
+	help = "print json output from parser", default = False, dest = "json")
+argsparser.add_argument("--ninja", action = "store_true",
+	help = "print ninja output from engine", default = False, dest = "ninja")
+args = vars(argsparser.parse_args())
 
 # TODO clean up temporary ninja files in current working dir
 
-if any_failed:
+results = [run_test(test_filename.replace("\\", "/"), args.get("json"), args.get("ninja"))
+	for test_filename in glob.glob(args.get("in"))]
+
+if not all(results):
 	print("one or more tests failed")
-	sys.exit(1)
+	if not args.get("dry"):
+		sys.exit(1)
 else:
-	print("done")
+	print("all tests done")
