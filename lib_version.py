bedb735ee23d4f3d46379f40b8763a60a7558b16
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-10-06 20:16:03 +0200

    fix version check

--- lib_version.py
+++ lib_version.py
@@ -3,7 +3,7 @@
 import re
 
 MAJOR = 0
-MINOR = 0
+MINOR = 1
 VERSION = "%d.%d" % (MAJOR, MINOR)
 
 # Simple major.minor matcher
@@ -18,7 +18,7 @@ def parse(version):
 
 def check(required_version):
 	required_major, required_minor = parse(required_version)
-	if not required_major or not required_minor:
+	if not required_major and not required_minor:
 		raise ValueError("Specified required version (%s) has incorrect format." % required_version)
 	if MAJOR > required_major:
 		print("WARNING: BuildFox executable major version (%s) is greater than 'buildfox_required_version' (%s).\nVersions may be incompatible." % (VERSION, required_version))
