fc5f7d57fc8c918a821b4fb055fdf952b8feac26
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-10-07 23:06:13 +0200

    another try

--- tests/tests.py
+++ tests/tests.py
@@ -113,10 +113,18 @@ def run_test(test_filename, print_json = False, print_ninja = False):
 				print("--- JSON END -----------------")
 			if json_exists:
 				with open(json_filename, "r") as f:
+					reference = json.loads(f.read())
 					if sys.version_info[0] < 3:
-						reference = json.loads(f.read(), encoding = "latin-1")
-					else:
-						reference = json.loads(f.read())
+						def byteify(input):
+							if isinstance(input, dict):
+								return {byteify(key): byteify(value) for key,value in input.iteritems()}
+							elif isinstance(input, list):
+								return [byteify(element) for element in input]
+							elif isinstance(input, unicode):
+								return input.encode("utf-8")
+							else:
+								return input
+						reference = byteify(reference)
 				diff = DeepDiff(reference, engine.output)
 				if diff:
 					print("Results differ from reference:")
