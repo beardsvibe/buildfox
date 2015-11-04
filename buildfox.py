5208bae15c9012790ce9d9f7f9250eb48ab0e25b
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-11-04 15:53:30 +0100

    Add profiler code

--- buildfox.py
+++ buildfox.py
@@ -352,4 +352,13 @@ def main(*argv, **kwargs):
 				args.get("in"))
 
 if __name__ == "__main__":
+	#import cProfile, pstats, io
+	#pr = cProfile.Profile()
+	#pr.enable()
 	main()
+	#pr.disable()
+	#s = io.StringIO()
+	#sortby = "cumulative"
+	#ps = pstats.Stats(pr, stream = s).sort_stats(sortby)
+	#ps.print_stats()
+	#print(s.getvalue())
